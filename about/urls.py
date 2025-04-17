e59150c3a63ee1e5293e655360e49b6b30b5c1ca
JedDizon <ku98sovk@students.codeinstitute.net>
2025-04-17 16:51:48 +0100

    update about urls and views py to use data from blog model to display blog

--- about/urls.py
+++ about/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.about_me, name='about'),
+    # path('', views.about_me, name='about'),
+    path('', views.about_view, name='about'),
 ]
--- about/views.py
+++ about/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 from .models import About
+from blog.models import Post
 
+# def about_me(request):
+#     """
+#     Renders the About page
+#     """
+#     about = About.objects.all().order_by('-updated_on').first()
 
-def about_me(request):
-    """
-    Renders the About page
-    """
-    about = About.objects.all().order_by('-updated_on').first()
+#     return render(
+#         request,
+#         "about/about.html",
+#         {"about": about},
+#     )
 
+def about_view(request):
+    post = Post.objects.filter(status=1).order_by('-created_on').first()
     return render(
         request,
-        "about/about.html",
-        {"about": about},
+        'about/about.html',
+        {'post': post},
     )
