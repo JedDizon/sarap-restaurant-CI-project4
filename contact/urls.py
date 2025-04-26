ee64c4d8690193543f67d5cffbbc674c0cc95d6d
JedDizon <ku98sovk@students.codeinstitute.net>
2025-04-26 11:11:34 +0100

    added name to each form element, added POST method and csrf token, updated views py to get input from form and provide feedback to user

--- contact/urls.py
+++ contact/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.contact_render, name='contact'),
+    path('', views.contact_page, name='contact'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,15 +1,48 @@
 from django.shortcuts import render
 from .models import Contact
+# from django.http import HttpResponse
+from django.contrib import messages
+from django.shortcuts import redirect
+
+# def contact_render(request):
+#     """
+#     Renders the Contact page
+#     """
+#     contact = Contact.objects.all().order_by('-updated_on').first()
+
+#     return render(
+#         request,
+#         "contact/contact.html",
+#         {"contact": contact},
+#     )
+
+# def contact_page(request):
+#     contact = Contact.objects.all().order_by('-updated_on').first()
+
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         email = request.POST.get('email')
+#         phone = request.POST.get('phone')
+#         subject = request.POST.get('subject')
+#         message = request.POST.get('message')
+#         print(f"Received message from {name} ({email}): {subject} - {message}")
+#         return HttpResponse("Message received!")
+
+#     return render(request, "contact/contact.html", {"contact": contact})
 
 
-def contact_render(request):
-    """
-    Renders the Contact page
-    """
+def contact_page(request):
     contact = Contact.objects.all().order_by('-updated_on').first()
 
-    return render(
-        request,
-        "contact/contact.html",
-        {"contact": contact},
-    )
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        print(f"Received message from {name} ({email} - {phone}): {subject} - {message}")
+        messages.success(request, "Thanks! Your message has been received.")
+        return redirect('contact')  # Redirect to avoid form resubmission
+
+    return render(request, "contact/contact.html", {"contact": contact})
