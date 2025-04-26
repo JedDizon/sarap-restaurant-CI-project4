c3158a2fc607d3d8771d0b5b127b32136dbb6723
JedDizon <ku98sovk@students.codeinstitute.net>
2025-04-26 12:53:59 +0100

    made contact form functional. It will send email to sender and owner, updated layout of header, added logo, updated navbar to show active html

---
+++ staticfiles/js/contact.js
@@ -0,0 +1,21 @@
+function sendMail(contactForm) {
+    emailjs.send("service_c8ydk5f_sarap","template_jjsojjq", {
+        "name": contactForm.name.value,
+        "subject": contactForm.subject.value,
+        "message": contactForm.message.value,
+        "email": contactForm.email.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+            alert("Your message has been sent successfully!");
+            contactForm.reset();
+        },
+        function(error) {
+            console.log("FAILED", error);
+            alert("Failed to send the message. Please try again later.");
+        }
+    );
+    // 🔥 Prevent the default form POST
+    return false;
+}
