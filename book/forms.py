2b03cda97a4037d096d876c722ed8e5094d12c85
JedDizon <ku98sovk@students.codeinstitute.net>
2025-05-02 17:58:38 +0100

    add user able to update booking

--- book/forms.py
+++ book/forms.py
@@ -1,10 +1,10 @@
-from django import forms
 from .models import Reservation
+from django import forms
 
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
-        fields = ['requested_date', 'requested_time', 'seats']
+        fields = ['requested_date', 'requested_time', 'seats', 'guest_name', 'guest_phone']
         widgets = {
             'requested_date': forms.DateInput(attrs={'type': 'date'}),
             'requested_time': forms.TimeInput(attrs={'type': 'time'}),
--- book/views.py
+++ book/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from .models import Book, Reservation
+from django.shortcuts import get_object_or_404, render
+from .models import Reservation, Book
 from .forms import ReservationForm
 
 def booking_render(request):
@@ -27,4 +27,57 @@ def booking_render(request):
         "form": form,
         "success": success,
         "reservations": reservations,
+    })
+
+def booking_render(request):
+    book = Book.objects.all().order_by('-updated_on').first()
+    form = None
+    success = False
+    edit_id = request.POST.get('edit_id') 
+    edit_form = None
+    reservations = None
+
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if 'edit_id' in request.POST:
+                reservation = get_object_or_404(Reservation, pk=edit_id)
+
+                if reservation.user != request.user and not request.user.is_staff:
+                    return render(request, "book/book.html", {
+                        "book": book,
+                        "error": "You do not have permission to edit this reservation.",
+                    })
+
+                if reservation.status == 'declined':
+                    return render(request, "book/book.html", {
+                        "book": book,
+                        "error": "This reservation was declined and cannot be modified.",
+                    })
+
+                edit_form = ReservationForm(request.POST, instance=reservation)
+                if edit_form.is_valid():
+                    updated = edit_form.save(commit=False)
+                    if reservation.status == 'approved':
+                        updated.status = 'pending'
+                    updated.save()
+                    success = True
+            else:
+                form = ReservationForm(request.POST)
+                if form.is_valid():
+                    reservation = form.save(commit=False)
+                    reservation.user = request.user
+                    reservation.save()
+                    success = True
+        else:
+            form = ReservationForm()
+
+        reservations = Reservation.objects.filter(user=request.user).order_by('-created_date')
+
+    return render(request, "book/book.html", {
+        "book": book,
+        "form": form,
+        "success": success,
+        "reservations": reservations,
+        "edit_form": edit_form,
+        "edit_id": edit_id,
     })
