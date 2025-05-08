a8e1c3761dc76e779585ce0d712d197e939abba6
JedDizon <ku98sovk@students.codeinstitute.net>
2025-05-08 11:24:34 +0100

    add menu model to allow admins to delete, update, create menu items

--- menu/admin.py
+++ menu/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models import Menu
 from django_summernote.admin import SummernoteModelAdmin
+from .models import MenuItem
 
 
 @admin.register(Menu)
 class MenuAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('content',)
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'cost')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
--- menu/models.py
+++ menu/models.py
@@ -8,3 +8,32 @@ class Menu(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class MenuItem(models.Model):
+    CATEGORY_CHOICES = [
+        ('Starter', 'Starter'),
+        ('Main', 'Main'),
+        ('Soup', 'Soup'),
+        ('Side', 'Side'),
+        ('Dessert - Traditional', 'Dessert - Traditional'),
+        ('Dessert - Other', 'Dessert - Other'),
+        ('Drink - Hot', 'Drink - Hot'),
+        ('Drink - Cocktail', 'Drink - Cocktail'),
+        ('Drink - Nonalcoholic', 'Drink - Nonalcoholic'),
+        ('Drink - Spirit', 'Drink - Spirit'),
+        ('Drink - Beer', 'Drink - Beer'),
+        ('Drink - Mixer', 'Drink - Mixer'),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    cost = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='items')
+
+    class Meta:
+        ordering = ['category', 'name']
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
