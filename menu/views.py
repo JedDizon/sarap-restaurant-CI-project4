c4ece1882755210f7cfe323f89cde306e0becc53
JedDizon <ku98sovk@students.codeinstitute.net>
2025-05-09 18:09:23 +0100

    update menu html to display menu items, styled with bootstrap, added images

--- menu/views.py
+++ menu/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
 from .models import Menu
 
-
 def menu_render(request):
     """
-    Renders the Menu page
+    Renders the Menu page with related items
     """
     menu = Menu.objects.all().order_by('-updated_on').first()
+
+    if menu:
+        items = menu.items.all()
+        starters = items.filter(category='Starter')
+        mains = items.filter(category='Main')
+        soups = items.filter(category='Soup')
+        sides = items.filter(category='Side')
+        desserts = items.filter(category__startswith='Dessert')
+        drinks = items.filter(category__startswith='Drink')
+    else:
+        starters = mains = soups = sides = desserts = drinks = []
 
     return render(
         request,
         "menu/menu.html",
-        {"menu": menu},
+        {
+            "menu": menu,
+            "starters": starters,
+            "mains": mains,
+            "soups": soups,
+            "sides": sides,
+            "desserts": desserts,
+            "drinks": drinks,
+        },
     )
